31bdefbe7f59231301f22b32159f89192435a480
olamide <myyde8170@gmail.com>
2022-03-13 23:22:42 +0100

    added path for index

--- appy/urls.py
+++ appy/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     # path("january", views.index),
     # path("feb", views.celeb)
+    path("", views.index),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.challenge, name="month_challenge")
 
